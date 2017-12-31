5c4c0134f353eade49ae593ce2e1ec50adec06fe
Alexander Mazyrin <algoritmist1618@gmail.com>
2017-12-31 16:05:29 +0300

    [+] Finish scheduling problem. [+] Add scheduling and MST problems descriptions.

--- README.md
+++ README.md
@@ -8,5 +8,7 @@ List of algorithms in C++:
 3) Quicksort. Recursive, in place, median of three pivot element selection strategy.
 4) Karger's algorithm. Randomized algorithm to compute minimum cut of a connected graph.
 5) Kosaraju's algorithm - Strongly Connected Components (SCC).
-6) Dijkstra's algorithm. "Naive" implementation (whitout heap).
+6) Dijkstra's algorithm. Straightforward implementation (whitout heap).
 7) Median maintanence. Two heaps.
+8) Prim's MST Algorithm. Straightforward implementation (whitout heap).
+9) Scheduling problem. Minimizing the weighted sum of completion times of a banch of tasks.
--- scheduling.cpp
+++ scheduling.cpp
@@ -2,50 +2,96 @@
 #include <fstream>
 #include <cstdlib>
 #include <queue>
+#include <vector>
+#include <algorithm>
 
 using namespace std;
 
+typedef double task_score;
+
 struct task {
-	uint16_t id;
+	task(task_score score, int16_t weight, int16_t lenght)
+		: score(score), weight(weight), lenght(lenght) {}
+	task_score score;
 	int16_t weight;
 	int16_t lenght;
-}
+};
 
-typedef int16_t score;
-
-//typedef priority_queue<score, task, less<uint16_t>> max_heap;
 typedef vector<task> scheduled_list;
+
+task_score calculate_score(int16_t weight, int16_t lenght);
+bool compare(task l, task r);
 
 int main(int argc, char *argv[])
 {
 	if (argc < 2) {
-		cerr << "Err params! Usage: " << argv[0] << "INFILE" << endl;
+		cerr << "Err params! Usage: " << argv[0] << " INFILE" << endl;
 		return -1;
 	}
 
 	ifstream fi(argv[1]);
 	uint16_t jobs_number;
 	int16_t weight, lenght;
-	score job_score;
-	//max_heap scheduled_list;
+	uint32_t completion_time = 0;
+	uint64_t sum = 0;
+	task_score score;
+	scheduled_list sch_list;
 
 	fi >> jobs_number;
 #ifdef DEBUG
 	cout << "Number of jobs is " << jobs_number << endl;
 #endif
-	// reading jobs
+	// create unsorted list of jobs
 	for (int i = 0; i < jobs_number; ++i) {
 		fi >> weight >> lenght;
-		// calc score here
-		score = weight - lenght;
-		// fill scheduled list (max heap) here
-		scheduled_list.push(task(i, weight, lenght));
+		// calc score
+		score = calculate_score(weight, lenght);
+		// fill scheduled list
+		sch_list.push_back(task(score, weight, lenght));
 #ifdef DEBUG
-	cout << "Job: Id - " << i << ", Weight - " << weight <<
-	       	", lenght - " << lenght << ", score - " << score << endl;
+		cout << weight << " " << lenght << " " << score << endl;
 #endif
 	}
 
-	// print scheduled list here
+	// sort list of jobs by score and weight
+	sort(sch_list.begin(), sch_list.end(), compare);
+
+#ifdef DEBUG
+	// print scheduled list
+	cout << "Scheduled list: " << endl;
+	for (auto job : sch_list) {
+		cout << job.weight << " " << job.lenght << " "
+			<< job.score << endl;
+	}
+#endif
+	// calculate weighted sum
+	for (auto job : sch_list) {
+		completion_time += job.lenght;
+		sum += job.weight * completion_time;
+	}
+	cout << "Sum is " << sum << endl;
+
 	return 0;
+}
+
+task_score calculate_score(int16_t weight, int16_t lenght)
+{
+#ifdef SUB
+	return weight - lenght;
+#else
+	return (double)weight / (double)lenght;
+#endif
+}
+bool compare(task l, task r)
+{
+	// score is primary condition
+	if (l.score > r.score)
+		return true;
+	if (l.score < r.score)
+		return false;
+	// if scores are equal check weights
+	if (l.weight > r.weight)
+		return true;
+	if (l.weight < r.weight)
+		return false;
 }
