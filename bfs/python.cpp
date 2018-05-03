9d012db270f48fefd931e4227071eb290318ea33
Alexander Mazyrin <a.mazyrin@partner.samsung.com>
2018-05-03 09:41:41 +0300

    bfs: improve python, dfs: imptove gifts

--- bfs/python.cpp
+++ bfs/python.cpp
@@ -6,6 +6,7 @@ using namespace std;
 #define N_MAX 15
 #define M_MAX 15
 #define PYTHON_LEN_MAX 9
+#define V_MAX 65536 // 4^8
 
 struct Point {
 	short x;
@@ -18,10 +19,9 @@ struct Python {
 };
 
 int field[N_MAX + 2][M_MAX + 2];
-Python queue[N_MAX * M_MAX * 65536];
-bool visited[N_MAX][M_MAX][65536];
-int dist[N_MAX][M_MAX][65536];
-int head, tail;
+Python queue[N_MAX * M_MAX * V_MAX];
+int qhead, qtail;
+int dist[N_MAX][M_MAX][V_MAX];
 Point moves[MOVES_NUMBER] = {{-1, 0}, {1, 0}, {0, 1}, {0, -1}};
 
 int get_move_bit(Point &mv)
@@ -66,7 +66,6 @@ bool tail_crash(const Point &head, Point *p, int p_len)
 	if (p_len == 2) {
 		k = 0;
 	}
-
 	for (int i = 1; i < p_len-k; ++i) {
 		if (head.x == p[i].x && head.y == p[i].y) {
 			res = true;
@@ -78,14 +77,21 @@ bool tail_crash(const Point &head, Point *p, int p_len)
 
 int main()
 {
-	int n, m;
+	int n, m, p_len = 0, res = -1;
 	char ch;
 	Point C, python[PYTHON_LEN_MAX];
-	int p_len = 0;
 	Python p_start;
+	bool found = false;
 
 	cin >> n >> m;
 
+	for (int i = 0; i < N_MAX; ++i) {
+		for (int j = 0; j < M_MAX; ++j) {
+			for (int k = 0; k < V_MAX; ++k) {
+				dist[i][j][k] = -1;
+			}
+		}
+	}
 	for (int i = 0; i < n+2; ++i) {
 		for (int j = 0; j < m+2; ++j) {
 			if (i == 0 || i == n+1 || j == 0 || j == m+1) {
@@ -114,33 +120,46 @@ int main()
 	}
 
 	python_pack(python, p_len, p_start);
-	queue[tail++] = p_start;
-	visited[p_start.head.x-1][p_start.head.y-1][p_start.tail] = true;
+	queue[qtail++] = p_start;
+	dist[p_start.head.x-1][p_start.head.y-1][p_start.tail] = 0;
 
-	while (head != tail) {
-		Python p_c = queue[head++];
+	while (qhead != qtail) {
+		Python p_c = queue[qhead++];
 		python_unpack(python, p_len, p_c);
 
 		for (int i = 0; i < MOVES_NUMBER; ++i) {
 			Python p_n;
-			Point &h_n = p_n.head; // head next
-			Point &h_c = p_c.head; // head current
+			Point &h_n = p_n.head;
+			Point &h_c = p_c.head;
 			h_n.x = h_c.x + moves[i].x;
 			h_n.y = h_c.y + moves[i].y;
-			// TODO
-			if (field[h_n.x][h_n.y] == -1 && !tail_crash(h_n, python, p_len)) {
-				unsigned short &t_n = p_n.tail; // tail next
-				t_n = p_c.tail >> 2;
-				Point move_op;
-				move_op.x = moves[i].x * (-1);
-				move_op.y = moves[i].y * (-1);
-				t_n |= get_move_bit(move_op) << ((p_len-2) * 2);
-				queue[tail++] = p_n;
-				field[h_n.x][h_n.y] = field[h_c.x][h_c.y] + 1;
+
+			if (field[h_n.x][h_n.y] == -2 || tail_crash(h_n, python, p_len)) {
+				continue;
 			}
+
+			unsigned short &t_n = p_n.tail;
+			t_n = p_c.tail >> 2;
+			Point move_op;
+			move_op.x = moves[i].x * (-1);
+			move_op.y = moves[i].y * (-1);
+			t_n |= get_move_bit(move_op) << ((p_len-2) * 2);
+
+			if (dist[h_n.x-1][h_n.y-1][t_n] == -1) {
+				queue[qtail++] = p_n;
+				dist[h_n.x-1][h_n.y-1][t_n] = dist[h_c.x-1][h_c.y-1][p_c.tail] + 1;
+				if (h_n.x == C.x && h_n.y == C.y) {
+					found = true;
+					res = dist[h_n.x-1][h_n.y-1][t_n];
+					break;
+				}
+			}
+		}
+		if (found) {
+			break;
 		}
 	}
 
-	cout << field[C.x][C.y] << endl;
+	cout << res << endl;
 	return 0;
 }
