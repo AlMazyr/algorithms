76e4993b93fe82da0a5ebc078790cf9f01a8a2b5
Alexander Mazyrin <algoritmist1618@gmail.com>
2018-05-05 16:55:05 +0300

    assembly: merging

--- dfs/assembly.cpp
+++ dfs/assembly.cpp
@@ -1,25 +1,5 @@
 #include <iostream>
 
-<<<<<<< HEAD
-using namespace std;
-
-#define N_MAX 1000000
-
-int *adj_list[N_MAX];
-
-struct Edge {
-	int v1;
-	int v2;
-};
-
-int main()
-{
-	int N;
-
-	cin >> N;
-	cout << N << endl;;
-
-=======
 #define N_MAX 1000000
 
 using namespace std;
@@ -35,42 +15,16 @@ int main()
 	cin >> N;
 
 	// preparing input data
->>>>>>> a521592fd4c5f28e848203bf4a039e160fe1844f
 	while (1) {
 		int v1, v2;
 		cin >> v1 >> v2;
 		if (cin.eof())
 			break;
-<<<<<<< HEAD
-		cout << v1 << " " << v2 << endl;
-
-		int adj_sz;
-		if (adj_list[v1-1] == NULL) {
-			adj_sz = 1;
-			adj_list[v1-1] = new int[adj_sz+1];
-			adj_list[v1-1][1] = v2;
-		} else {
-			adj_sz = adj_list[v1-1][0];
-			adj_sz += 1;
-			int *adj_new = new int[adj_sz+1];
-			for (int i = 1; i < adj_sz; ++i) {
-				adj_new[i] = adj_list[v1-1][i];
-			}
-			delete adj_list[v1-1];
-			adj_list[v1-1] = adj_new;
-			adj_list[v1-1][adj_sz] = v2;
-		}
-		adj_list[v1-1][0] = adj_sz;
-	}
-
-	//print adj_list
-=======
 		v_adj_num[v1-1]++;
 		edges[edges_num][0] = v1;
 		edges[edges_num][1] = v2;
 		edges_num++;
 	}
-
 	for (int i = 0; i < edges_num; ++i) {
 		int &v1 = edges[i][0], &v2 = edges[i][1];
 		if (adj_list[v1-1] == NULL) {
@@ -84,7 +38,6 @@ int main()
 	}
 
 	// print adj list
->>>>>>> a521592fd4c5f28e848203bf4a039e160fe1844f
 	for (int i = 0; i < N; ++i) {
 		cout << i+1 << " -> ";
 		if (adj_list[i] == NULL) {
