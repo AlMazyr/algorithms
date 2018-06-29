62453153ffc93c511faae6c6b16c68b7e09152e1
Alexander Mazyrin <a.mazyrin@partner.samsung.com>
2018-06-29 18:05:38 +0300

    sw_tests: toilet: add test generator
    Signed-off-by: Alexander Mazyrin <a.mazyrin@partner.samsung.com>

---
+++ sw_tests/toilet/toilet_gen.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+tests_num = 1
+
+N = 20000
+M = 20000
+
+print(tests_num)
+for i in range(tests_num):
+    print("{} {}".format(N, M))
+    for j in range(M):
+        print("{} {}".format(1, j))
+
